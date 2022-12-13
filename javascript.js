afa1cca0ed2555a6be382b2f832fd18154fe596f
Duck <118816921+ducke666@users.noreply.github.com>
2022-12-13 12:34:57 +0000

    Build initial JS logic for Rock Paper Scissors game with bug control

--- javascript.js
+++ javascript.js
@@ -1,3 +1,34 @@
-let userChoice = '';
-let computerChoice = '';
+// Create empty user choice variable 
+let userChoice = 'empty';
+// Generate & store Computer random number between 1 - 3
+let computerChoiceNum = Math.floor(Math.random() * 3 + 1);
+console.log(`1: ${computerChoiceNum}`);
 
+// Var to store computer choice string
+let computerChoiceString = 'empty1';
+
+// Collect user choice string and store in userChoice variable 
+function getUserChoice() {
+    userChoice = (prompt("Enter either: Sock, Paper or Rizzla"))
+    userChoice = userChoice.toLowerCase();
+}
+
+// Convert the computer choice number to a string
+function convertCompChoice(computerChoiceNum) {
+    if (computerChoiceNum === 1) {
+        computerChoiceString = 'sock';
+    } else if (computerChoiceNum === 2) {
+        computerChoiceString = 'paper';
+    } else if (computerChoiceNum === 3) {
+        computerChoiceString = 'rizzla';
+    } else {
+        console.log('error with computer choice');
+    };
+}
+
+
+
+getUserChoice();
+convertCompChoice(computerChoiceNum);
+console.log(`2: ${userChoice}`);
+console.log(`3: ${computerChoiceString}`);
